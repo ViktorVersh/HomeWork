17659827ff495efb93cbf72e36e65e1b374adedc
ViktorVersh <vershininzaig@mail.ru>
2024-09-27 00:48:35 +0800

    закончил работу

---
+++ My_work/module_4/fake_math.py
@@ -0,0 +1,12 @@
+def divide(first, second):
+    if second == 0:
+        return "Ошибка"
+    else:
+        div = first / second
+        return div
+
+
+# result = divide(69, 3)
+# print(result)
+# result_1 = divide(54, 0)
+# print(result_1)
