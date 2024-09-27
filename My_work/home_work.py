49ac9c612a97804167a22a7b9842aff4c5f5a0fc
ViktorVersh <vershininzaig@mail.ru>
2024-09-27 21:20:09 +0800

    изменил строку print

--- My_work/home_work.py
+++ My_work/home_work.py
@@ -1,35 +1,34 @@
-def sum_numbers_and_string_lengths(data_structure):
-    itog_sum = 0
-    for i in data_structure:
-        if isinstance(i, int):
-            itog_sum += i
-        elif isinstance(i, str):
-            itog_sum += len(i)
-        elif isinstance(i, (list, tuple, set)):
-            inner_sum = sum_numbers_and_string_lengths(i)
-            itog_sum += inner_sum
-        elif isinstance(i, dict):
-            for key, value in i.items():
-                if isinstance(key, str):
-                    itog_sum += len(key)
-                elif isinstance(key, int):
-                    itog_sum += key
-                if isinstance(value, int):
-                    itog_sum += value
-                elif isinstance(value, str):
-                    itog_sum += len(value)
-                elif isinstance(value, (list, tuple, set)):
-                    inner_sum = sum_numbers_and_string_lengths(value)
-                    itog_sum += inner_sum
-                elif isinstance(value, dict):
-                    inner_sum = sum_numbers_and_string_lengths([value])
-                    itog_sum += inner_sum
-    return itog_sum
-
-
 data_structure = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello",
                   ((), [{(2, 'Urban', ('Urban2', 35))}])]
 
 
-value_sum = sum_numbers_and_string_lengths(data_structure)
-print(value_sum)
+def sum_numbers_and_string_lengths(data_structure):
+    result_sum = 0
+    for i in data_structure:
+        if isinstance(i, int):
+            result_sum += i
+        elif isinstance(i, str):
+            result_sum += len(i)
+        elif isinstance(i, (list, tuple, set)):
+            inter_sum = sum_numbers_and_string_lengths(i)
+            result_sum += inter_sum
+        elif isinstance(i, dict):
+            for key, value in i.items():
+                if isinstance(key, str):
+                    result_sum += len(key)
+                elif isinstance(key, int):
+                    result_sum += key
+                if isinstance(value, int):
+                    result_sum += value
+                elif isinstance(value, str):
+                    result_sum += len(value)
+                elif isinstance(value, (list, tuple, set)):
+                    inter_sum = sum_numbers_and_string_lengths(value)
+                    result_sum += inter_sum
+                elif isinstance(value, dict):
+                    inter_sum = sum_numbers_and_string_lengths([value])
+                    result_sum += inter_sum
+    return result_sum
+
+
+print(sum_numbers_and_string_lengths(data_structure))
