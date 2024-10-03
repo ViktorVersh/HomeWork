14e5c42d2d6525d51cdf9a86d0f0d7fb21529366
ViktorVersh <vershininzaig@mail.ru>
2024-10-03 18:05:52 +0800

    Update module_4_2.py
    отредактировал комментарий

--- module_4_2.py
+++ module_4_2.py
@@ -6,6 +6,8 @@ def test_function():
 
 
 test_function()
-
-# inner_function() # - Выдает ошибку name 'inner_function' is not defined. Did you mean: 'test_function'?
-# - Имя 'inner_function' не определено. Вы имели в виду: 'test_function'?
+"""
+вызов inner_function() вне функции test_function() - Выдает ошибку:
+name 'inner_function' is not defined. Did you mean: 'test_function'?
+имя 'inner_function' не определено. Вы имели в виду: 'test_function'?
+"""
