92cf3fcb1be9d170df290333a17c5bcbbcb8d61f
ViktorVersh <vershininzaig@mail.ru>
2024-10-12 17:55:34 +0800

    лекции 5 модуль

--- module_5/module_5.py
+++ module_5/module_5.py
@@ -12,6 +12,30 @@ class Human:
     def birthday(self):
         self.age += 1
         print(f'У меня день рождения, мне теперь {self.age}')
+
+    def __del__(self):
+        print(f'{self.name} ушел')
+
+    def __len__(self):
+        return self.age
+
+    def __lt__(self, other): # lt - Lower then - меньше чем
+        return self.age < other.age
+
+    def __gt__(self, other): # gt - Greater then - Больше чем
+        return  self.age > other.age
+
+    def __eq__(self, other): # Оператор равенства
+        return self.name == other.name and self.age == other.age
+
+    def __bool__(self):
+        return bool(self.age)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+    
 '''
 объект это переменная созданная внутри класса, а класс это инструкция (план) в котором описаны какие характеристики,
 способности есть у наших объектов
@@ -20,10 +44,20 @@ class Human:
 
 
 den = Human('Денис', 22)
-maxi = Human('Максим', 25)
+maxi = Human('Максим', 22)
+
+if den:
+    den.say_info()
+print(den)
+print(den < maxi)
+print(den > maxi)
 maxi.birthday()
-print(den.name, den.age)
-print(maxi.name, maxi.age)
-print(den.name, den.age, maxi.name, maxi.age)
-den.say_info()
-maxi.say_info()
+print(den < maxi)
+print(den > maxi)
+print(den == maxi)
+# print(len(den))
+# print(den.name, den.age)
+# print(maxi.name, maxi.age)
+# print(den.name, den.age, maxi.name, maxi.age)
+# den.say_info()
+# maxi.say_info()
