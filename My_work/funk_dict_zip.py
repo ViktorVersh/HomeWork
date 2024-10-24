a230bc287a6f58ebe4f0677a937be76a7f27b0a6
ViktorVersh <vershininzaig@mail.ru>
2024-10-24 13:14:19 +0800

    изменил функции, добавил комментарий

--- My_work/funk_dict_zip.py
+++ My_work/funk_dict_zip.py
@@ -3,19 +3,17 @@ emplo_camp = ['Виктор', 'Николай', 'Василий', 'Максим'
 campain = dict(zip(emplo_camp, salary_camp))
 
 
-def avg_salary():
+def avg_salary():  # Возвращает средний заработной платы через переменную
     avg_sal = round(sum(salary_camp) / len(salary_camp), 2)
     return avg_sal
 
 
-def max_salary():
-    max_sal = round(max(salary_camp), 2)
-    return max_sal
+def max_salary():  # Возвращает максимальную зарплату через функцию
+    return round(max(salary_camp), 2)
 
 
-def min_salary():
-    min_sal = round(min(salary_camp), 2)
-    return min_sal
+def min_salary():  # Возвращает максимальную зарплату через функцию
+    return round(min(salary_camp), 2)
 
 
 print(campain)
