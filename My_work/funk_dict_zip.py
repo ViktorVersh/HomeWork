5bba38b013cfd47496c69b0b056bff3b84002b3b
ViktorVersh <vershininzaig@mail.ru>
2024-10-24 12:45:15 +0800

    как func_buil.py ч/з функции

---
+++ My_work/funk_dict_zip.py
@@ -0,0 +1,26 @@
+salary_camp = [6800, 5460.25, 6400.2456, 3200.1556, 8000.452]
+emplo_camp = ['Виктор', 'Николай', 'Василий', 'Максим', 'Данил']
+campain = dict(zip(emplo_camp, salary_camp))
+
+
+def avg_salary():
+    avg_sal = round(sum(salary_camp) / len(salary_camp), 2)
+    return avg_sal
+
+
+def max_salary():
+    max_sal = round(max(salary_camp), 2)
+    return max_sal
+
+
+def min_salary():
+    min_sal = round(min(salary_camp), 2)
+    return min_sal
+
+
+print(campain)
+print(campain[emplo_camp[3]])
+print(campain['Василий'])
+print(f'Максимальная зарплата в кампании: {max_salary()}')
+print(f'Минимальная зарплата в кампании: {min_salary()}')
+print(f'Средняя зарплата в кампании: {avg_salary()}')
