79c1928cb741f6ff77db766fbfeb3c7a8741481e
ViktorVersh <vershininzaig@mail.ru>
2024-10-25 12:57:48 +0800

    PEP8

--- My_work/Module_7/module7.py
+++ My_work/Module_7/module7.py
@@ -1,37 +1,28 @@
 from pprint import pprint
 
-
 name = 'module_txt.txt'  # просто текстовый файл
-file = open(name, 'r', encoding = 'UTF-8')  # r = read, w = write, a = append
+file = open(name, 'r', encoding='UTF-8')  # r = read, w = write, a = append
 print(file)
 pprint(file.read())
 file.close()
 
 name = 'module_txt2.txt'
-file = open(name, 'w', encoding = 'UTF-8')
+file = open(name, 'w', encoding='UTF-8')
 file.write('Здравствуйте!!!\n Меня зовут Виктор\n Мне 57 лет \n Я учусь в университете Urban\n')
 file.close()
 
 name = 'module_txt2.txt'
-file = open(name, 'a', encoding = 'UTF-8')
+file = open(name, 'a', encoding='UTF-8')
 file.write('\nHello, world!')
 file.close()
 
 name = 'module_txt2.txt'
-file = open(name, 'r', encoding = 'UTF-8')
+file = open(name, 'r', encoding='UTF-8')
 pprint(file.read())
 file.close()
 
-# name = 'module_txt2.txt'
-# file = open(name, 'r', encoding = 'UTF-8')
-# pprint(file.tell())
-# pprint(file.read())
-# file.close()
-
 name = 'module_txt2.txt'
-file = open(name, 'a', encoding = 'UTF-8')
+file = open(name, 'a', encoding='UTF-8')
 print(file.seek(30))
 file.write('\nСегодня ясный солнечный день\n')
-# pprint(file.read())
 file.close()
-
