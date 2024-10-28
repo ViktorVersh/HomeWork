a86af5027bfbda3583af1c0f3586798004dc4d27
ViktorVersh <vershininzaig@mail.ru>
2024-10-28 23:10:56 +0800

    практика модуль 4.2

--- My_work/module_4/sortfunc.py
+++ My_work/module_4/sortfunc.py
@@ -1,28 +1,21 @@
-nums = [5, 6, 3, 2, 1, 4, 10, 12, 11, 7, 16, 8, 13, 9, 14, 15]
-
-
 def buble_sort(ls):  # - пузырьковая сортировка "большая О=n^2"
     swapped = True
     while swapped:
         swapped = False
         for i in range(len(ls) - 1):
-            if ls[i] > ls[i + 1]:
+            if ls[i] > ls[i+1]:
                 ls[i], ls[i + 1] = ls[i + 1], ls[i]
                 swapped = True
 
 
-buble_sort(nums)
-print(nums)
-
-
-def section_sort(ls):  # сортировка выборкой
-    for i in range(len(ls)):
-        lowest = i
-        for j in range(i + 1, len(ls)):
-            if ls[j] < ls[lowest]:
-                lowest = j
-                ls[i], ls[lowest] = ls[lowest], ls[i]
-
-
-section_sort(nums)
-print(nums)
+def selection_sort(ls2):  # сортировка выборкой
+    swap = True
+    while swap:
+        swap = False
+        for i in range(len(ls2)):
+            lowest = i
+            for j in range(i + 1, len(ls2)):
+                if ls2[j] < ls2[lowest]:
+                    lowest = j
+                    ls2[i], ls2[lowest] = ls2[lowest], ls2[i]
+                    swap = True
