ec26561709353c5d9a0fefbbb5840c027d94e77b
ViktorVersh <vershininzaig@mail.ru>
2024-11-05 18:24:58 +0800

    домашнее задание 1 модуль 8

---
+++ My_work/module_8/examles_1.py
@@ -0,0 +1,28 @@
+def f1(number):
+    return 10 / number
+
+
+def f2():
+    print('Какой хороший день!')
+    summ = 0
+    try:
+        for i in range(-2, 2):
+            summ += f1(i)
+            print(summ)
+        # return summ
+    except ZeroDivisionError as exc:
+        print(f'Вот что пошло не так - "{exc}", но программа ещё жива') # Отрабатывает ошибку внутри функции
+    return summ
+
+
+try:
+    total = f2()
+    print(total)
+    print(f'Вот ваш результат: {total}')
+    print(total / 0)
+
+except ZeroDivisionError as exc:
+    print(f'Вот что пошло не так - "{exc}", но мы устояли') # Отрабатывает ошибку вне функции
+
+
+
