b74dbfe6fb1812150cc274df1e05ad8937ac53f3
ViktorVersh <vershininzaig@mail.ru>
2024-11-10 21:46:47 +0800

    переход на другую строку

--- My_work/module_9/module_9_4.py
+++ My_work/module_9/module_9_4.py
@@ -1,7 +1,13 @@
-from random import choice
+# ---------------Задача "Функциональное разнообразие"---------------------
+from random import choice  # Импортируем из библиотеки random функцию choice
 
 
 class MysticBall:
+    """
+    Класс имитирующий магический шар.
+    При вызове объекта класса, будет возвращать случайное слово из списка.
+    """
+
     def __init__(self, *words):
         self.words = words
 
@@ -11,16 +17,16 @@ class MysticBall:
 
 first = 'Мама мыла раму'
 second = 'Рамена мало было'
-print(list(map(lambda x, y: x == y, first, second)))
+print(list(map(lambda x, y: x == y, first, second)))  # Сравниваем символы строки и выводим результат True/False
 
 
-def get_advanced_writer(file_name):
-    def write_everything(*data_set):
-        with open(file_name, 'w', encoding='utf-8') as f:
-            for i in data_set:
-                f.write(str(i))
+def get_advanced_writer(file_name):  # Функция создающая файл(если его нет) и функцию для записи в него
+    def write_everything(*data_set):  # Функция для записи в файл
+        with open(file_name, 'w', encoding='utf-8') as f:  # Открываем файл
+            for i in data_set:  # проходим по данным
+                f.write(str(i) + '\n')  # записываем в файл
 
-    return write_everything
+    return write_everything  # Возвращаем функцию
 
 
 write = get_advanced_writer('example.txt')
