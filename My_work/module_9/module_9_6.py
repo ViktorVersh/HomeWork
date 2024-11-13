0bb360994cdcac3464543bd67debdaf5f3765130
ViktorVersh <vershininzaig@mail.ru>
2024-11-13 19:55:27 +0800

    добавил комментарий

--- My_work/module_9/module_9_6.py
+++ My_work/module_9/module_9_6.py
@@ -4,10 +4,11 @@
 
 
 def all_variants(text):
-    len_text = len(text)
-    for i in range(1, len_text + 1):
-        for j in range(len_text - i + 1):
-            yield text[j: j + i]
+    len_text = len(text)  # создаем переменную с длиной строки
+    for i in range(1, len_text + 1):  # Проходимся циклом по всей длине строки, включая последний символ
+        for j in range(len_text - i + 1):  # проходим циклом по строке с позиции 0 до i включительно
+            # ключевое слово yield превращает функцию в генератор
+            yield text[j: j + i]  # возвращаем подстроку, начинающуюся с индекса j и имеющую длину i
 
 
 a = all_variants("abc")
