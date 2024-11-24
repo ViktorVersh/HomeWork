10a5ad5c27ea2f28efb30714a8668ba980506c1c
ViktorVersh <vershininzaig@mail.ru>
2024-11-24 16:58:43 +0800

    лекция 6 модуль 10

---
+++ My_work/module_10/lesson10_6.py
@@ -0,0 +1,35 @@
+import multiprocessing
+import time
+import threading
+
+counter = 0
+
+
+def first_worker(n):
+    global counter
+    for i in range(n):
+        counter += 1
+        time.sleep(1)
+        print('Первый рабочий изменил значение на', counter)
+
+
+def second_worker(n):
+    global counter
+    for i in range(n):
+        counter += 1
+        time.sleep(1)
+        print('Второй рабочий изменил значение на', counter)
+
+
+# thread1 = threading.Thread(target=first_worker, args=(10,))
+# thread2 = threading.Thread(target=second_worker, args=(15,))
+# thread1.start()
+# thread2.start()
+# thread1.join()
+# thread2.join()
+
+if __name__ == '__main__':
+    process1 = multiprocessing.Process(target=first_worker, args=(10,))
+    process2 = multiprocessing.Process(target=second_worker, args=(15,))
+    process1.start()
+    process2.start()
