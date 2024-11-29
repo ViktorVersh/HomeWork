d97245524cab97b4de7185286e74d69be4d3ef46
ViktorVersh <vershininzaig@mail.ru>
2024-11-30 00:18:36 +0800

    урок 1 модуль 11

---
+++ My_work/module_11/lesson1.py
@@ -0,0 +1,23 @@
+"""
+Usage:
+  pip <command> [options]
+
+Commands:
+  install   -   Установить пакет.
+  download  -   Загрузить пакет.
+  uninstall -   Удалить пакет.
+  freeze    -   Вывод установленных пакетов в формате требований Output installed packages in requirements format.
+  inspect   -   Проверьте среду Python..
+  list      -   Список установленных пакетов.
+  show      -   Показать информацию об установленных пакетах.
+  check     -   Проверьте, что установленные пакеты имеют совместимые зависимости..
+  config    -   Управление локальной и глобальной конфигурацией.
+  search    -   Поиск пакетов в PyPI.
+  cache     -   Проверка и управление кэшем окружением Pip.
+  index     -   Проверьте информацию, доступную в индексах пакетов..
+  wheel                       Build wheels from your requirements.
+  hash                        Compute hashes of package archives.
+  completion                  A helper command used for command completion.
+  debug                       Show information useful for debugging.
+  help                        Show help for commands.
+"""
