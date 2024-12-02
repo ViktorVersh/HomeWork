638819ab9de211be569ec58aef44d47c51e4c0f4
ViktorVersh <vershininzaig@mail.ru>
2024-12-02 13:33:56 +0800

    модуль 3 задание 2

--- My_work/повторение/module3/module_3_2.py
+++ My_work/повторение/module3/module_3_2.py
@@ -31,26 +31,3 @@ send_email('Вы видите это сообщение как лучший ст
            sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
-
-"""
-
-Если же отправитель по умолчанию - university.help@gmail.com, то вывести сообщение: "Письмо успешно 
-отправлено с адреса <sender> на адрес <recipient>."
-В противном случае вывести сообщение: "НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <sender> 
-на адрес <recipient>."
-Здесь <sender> и <recipient> - значения хранящиеся в этих переменных.
-За один вызов функции выводится только одно и перечисленных уведомлений! Проверки перечислены по мере выполнения.
-
-Пример результата выполнения программы:
-Пример выполняемого кода (тесты):
-send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
-send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
-send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
-Вывод на консоль:
-Письмо успешно отправлено с адреса university.help@gmail.com на адрес vasyok1337@gmail.com
-НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса urban.info@gmail.com на адрес urban.fan@mail.ru
-Невозможно отправить письмо с адреса urban.teacher@mail.uk на адрес urban.student@mail.ru
-Нельзя отправить письмо самому себе!
-
-"""
