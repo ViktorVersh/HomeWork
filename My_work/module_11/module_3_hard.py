c8d5ccd1314a7dfcd920315740e865252d27d6ca
ViktorVersh <vershininzaig@mail.ru>
2024-12-05 16:00:16 +0800

    повторяю домашнее задание

--- My_work/module_11/module_3_hard.py
+++ My_work/module_11/module_3_hard.py
@@ -3,6 +3,11 @@ data_structure = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hel
 
 
 def calculate_structure_sum(data_structure):
+    """
+    Функция подсчета содержимого переменной data_structure
+    :param data_structure:
+    :return: Возвращает сумму ысех чисел, сумму длин строк, ключей и т.д.
+    """
     result_sum = 0
     for i in data_structure:
         if isinstance(i, int):
