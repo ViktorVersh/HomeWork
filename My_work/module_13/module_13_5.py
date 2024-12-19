9020b9469a31dfeae122e8e201908005c9f8ff6f
ViktorVersh <vershininzaig@mail.ru>
2024-12-19 20:17:48 +0800

    Домашнее задание 5 модуль 13

---
+++ My_work/module_13/module_13_5.py
@@ -0,0 +1,86 @@
+"""
+Задача "Меньше текста, больше кликов"
+"""
+from aiogram import Bot, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+api = 'Здесь должен быть токен'
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+kb.add(KeyboardButton('Расчитать'), KeyboardButton('Информация'))
+kb1 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+kb1.add(KeyboardButton('мужской'), KeyboardButton('женский'))
+
+
+class UserState(StatesGroup):
+    """
+    Класс состояний пользователя
+    """
+    age = State()
+    growth = State()
+    weight = State()
+    sex = State()
+
+
+@dp.message_handler(commands='start')
+async def start(message):  # Функция ответа на команду "start"
+    await message.answer("Привет! Я бот помогающий твоему здоровью. Введите команду 'Расчитать'",
+                         reply_markup=kb)  # Ответ бота, добавляем кнопки "Расчитать" и "Информация"
+
+
+@dp.message_handler(text='Расчитать')  # Обработка команды "Расчитать" и запуск машины состояний
+async def set_age(message):
+    await message.answer('Введите ваш возраст:')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)  # Обработка машины состояния после ввода возраста
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer('Введите ваш рост:')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)  # Обработка машины состояния после ввода роста
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer('Ведите ваш вес:')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)  # Обработка машины состояния после ввода веса
+async def set_sex(message, state):
+    await state.update_data(weight=message.text)
+    # Ответ бота, добавляем кнопки "мужской" и "женский"
+    await message.answer('Выберите ваш пол (мужской/женский):', reply_markup=kb1)
+    await UserState.sex.set()
+
+
+@dp.message_handler(state=UserState.sex)  # Обработка машины состояния после ввода пола
+async def send_calories(message, state):
+    await state.update_data(sex=message.text)
+    data = await state.get_data()
+    age = data['age']
+    growth = data['growth']
+    weight = data['weight']
+    sex = data['sex']
+    if sex == 'женский':
+        message.text = (10 * int(weight) + 6.25 * int(growth) - 5 * int(age) - 161)  # формула для женщин
+    else:
+        message.text = (10 * int(weight) + 6.25 * int(growth) - 5 * int(age) + 5)  # формула для мужчин
+
+    await message.answer(f'Ваша норма каллорий: {message.text}')
+    await state.finish()
+
+
+@dp.message_handler()
+async def all_message(message):  # Функция ответа на все сообщения
+    await message.answer('Введите команду /start, чтобы начать общение.')  # Ответ бота
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)  # Запуск бота
