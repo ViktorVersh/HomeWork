43e511b3586f3c975a2128109c6488d9bf930594
ViktorVersh <vershininzaig@mail.ru>
2024-12-31 01:25:20 +0800

    изменил вывод в консоль

--- My_work/повторение/matrix.py
+++ My_work/повторение/matrix.py
@@ -9,8 +9,16 @@ def get_matrix(n, m, value):
 
 
 result1 = get_matrix(2, 2, 10)
-result2 = get_matrix(3, 5, 42)
-result3 = get_matrix(4, 2, 13)
-print(result1)
-print(result2)
-print(result3)
+result2 = get_matrix(5, 5, 42)
+result3 = get_matrix(4, 3, 13)
+
+for i in result1:
+    print(i)
+print()
+
+for i in result2:
+    print(i)
+print()
+
+for i in result3:
+    print(i)
