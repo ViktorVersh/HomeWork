45fc1eb779aef69bd45526bcdad3edfb763951a1
ViktorVersh <vershininzaig@mail.ru>
2024-12-31 00:50:08 +0800

    исправлял подсчет веса

--- My_work/повторение/module_7/module_7_1.py
+++ My_work/повторение/module_7/module_7_1.py
@@ -1,28 +1,31 @@
 class Product:
     def __init__(self, name: str, weight: float, category: str):
         self.name = name
-        self.weigth = weight
+        self.weight = weight
         self.category = category
 
     def __str__(self):
-        return f'{self.name}, {self.weigth}, {self.category}'
+        return f'{self.name}, {self.weight}, {self.category}'
 
 
 class Shop:
     __file_name = 'products.txt'
 
+    def add(self, *products):
+        with open(self.__file_name, 'a', encoding='utf-8') as file:
+            for i in products:
+                # print(i)
+                if i.name not in self.get_products():
+                    file.write(f'{i}\n')
+                else:
+                    i.weight += float(self.get_products().split(',')[1])
+                    print(f'Продукт {i.name} уже был в магазине, его общий вес теперь равен {i.weight}')
+
+
     def get_products(self):
         with open(self.__file_name, 'r', encoding='utf-8') as file:
             products = file.read()
             return products
-
-    def add(self, *products):
-        with open(self.__file_name, 'a', encoding='utf-8') as file:
-            for i in products:
-                if i.name not in self.get_products():
-                    file.write(f'{i}\n')
-                else:
-                    print(f'Продукт {i.name} уже был в магазине, его общий вес теперь равен {i.weigth}')
 
 
 s1 = Shop()
