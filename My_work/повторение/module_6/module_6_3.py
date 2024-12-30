ce48f26cb420cc05730842470cbc547cc2c23e90
ViktorVersh <vershininzaig@mail.ru>
2024-12-30 13:38:03 +0800

    Задание 3 модуль 6

---
+++ My_work/повторение/module_6/module_6_3.py
@@ -0,0 +1,80 @@
+"""
+Задача: "Ошибка эволюции"
+"""
+import random
+
+
+class Animal:
+    _DEGREE_OF_DANGER = 0
+    live = True
+    sound = None
+    _cords = [0, 0, 0]
+    speed = 0
+
+    def move(self, dx, dy, dz):
+        self._cords[0] += dx * self.speed
+        self._cords[1] += dy * self.speed
+        if dz * self.speed < 0:
+            print("It's too deep, i can't dive :(")
+        else:
+            self._cords[2] += dz * self.speed
+
+    def get_cords(self):
+        print(f'X: {self._cords[0]} Y: {self._cords[1]} Z: {self._cords[2]}')
+
+    def attack(self):
+        if self._DEGREE_OF_DANGER < 5:
+            print("Sorry, i'm peaceful :)")
+        else:
+            print("Be careful, i'm attacking you 0_0")
+
+
+class Bird(Animal):
+    def __init__(self, beak = True):
+        self.beak = beak
+
+    def lay_eggs(self):
+        print(f"Here are(is) {random.randint(1,4)} eggs for you")
+
+
+class AquaticAnimal(Animal):
+    _DEGREE_OF_DANGER = 3
+
+    def dive_in(self, dz):
+        self._cords[2] -= dz * int(self.speed / 2)
+
+
+class PoisonousAnimal(Animal):
+    _DEGREE_OF_DANGER = 8
+
+
+class Duckbill(PoisonousAnimal, Bird, AquaticAnimal):
+    sound = "Click-click-click"
+    def __init__(self, speed):
+        self.beak = True
+        self.speed = speed
+        super().__init__()
+
+    def speek(self):
+        print(self.sound)
+
+
+db = Duckbill(10)
+
+print(db.live)
+print(db.beak)
+
+db.speek()
+db.attack()
+
+db.move(1, 2, 3)
+db.get_cords()
+db.dive_in(6)
+db.get_cords()
+
+db.lay_eggs()
+
+
+
+
+
