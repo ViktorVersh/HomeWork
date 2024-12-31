2ee80ad937e0874c1a10357e75d7744778008f58
ViktorVersh <vershininzaig@mail.ru>
2024-12-31 17:26:58 +0800

    повторял задание 1 модуль 8

---
+++ My_work/повторение/module_8/module_8_1.py
@@ -0,0 +1,17 @@
+"""
+Задание: Программистам можно всё
+"""
+
+def add_everything_up(a, b):
+    """
+    Функция, которая складывает две переменные
+    """
+    try:
+        return round((a + b), 3)
+    except TypeError:
+        return str(a) + str(b)
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
